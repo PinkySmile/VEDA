0b296f024f96ccbc87c8df51370272e8a150599c
Andgel <andgel.halley@epitech.eu>
2018-05-17 19:54:33 +0200

    Changed icon path

--- CSFML Version/include/sprites_conf.h
+++ CSFML Version/include/sprites_conf.h
@@ -9,7 +9,7 @@ const Sprite_config	sprites_conf[] = {
 	{"data/characters/1_character.png", {1, 1}, {16, 32}, {0, 0}},
 	{"data/characters/2_character.png", {1, 1}, {16, 32}, {0, 0}},
 	{"data/characters/3_character.png", {1, 1}, {16, 32}, {0, 0}},
-	{"data/godEater/godEater.png", {1, 1}, {32, 32}, {0, 0}},
+	{"data/icon/icon.png", {1, 1}, {32, 32}, {0, 0}},
 	{"data/life_bar.png", {1, 1}, {16, 16}, {0, 0}},
 	{"data/energy_bar.png", {1, 1}, {16, 16}, {0, 0}},
 	{"data/misc/cutscene.png", {1, 1}, {16, 16}, {0, 0}},
