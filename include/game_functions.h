6a68c09fd8caf25fe30a6ec61492ae026eab2814
Andgel <andgel.halley@epitech.eu>
2018-07-04 11:47:23 +0200

    Added id to make it clearer

--- include/game_functions.h
+++ include/game_functions.h
@@ -1,12 +1,12 @@
 #include "functions.h"
 
 void	(* const game_functions[])(game_t *game) = {
-	&mainMenu,
-	&inGame,
-	&controls,
-	&audio,
-	&options,
-	&buf_modif,
-	&changeName,
-	&battle,
+	&mainMenu,	//0
+	&inGame,	//1
+	&controls,	//2
+	&audio,		//3
+	&options,	//4
+	&buf_modif,	//5
+	&changeName,	//6
+	&battle,	//7
 };
