22b1519c283e13d57eb2d8d2c0fcb653e004db3b
Gegel85 <Gegel85@laposte.net>
2018-07-22 21:57:30 +0200

    Fixed save problems

--- src/saveFileMgr.c
+++ src/saveFileMgr.c
@@ -113,11 +113,13 @@ bool	saveGame(game_t *game, bool level)
 		for (int i = 0; i < game->characters.length; i++) {
 			charac = ((Character *)game->characters.content)[i];
 			memset(&obj, 0, sizeof(obj));
-			ParserObj_addElement(&obj, charac.name,						ParserStringType, "name");
-			ParserObj_addElement(&obj, &charac.movement.pos.x,				ParserIntType, "x_pos");
-			ParserObj_addElement(&obj, &charac.movement.pos.y,				ParserIntType, "y_pos");
-			ParserObj_addElement(&obj, &charac.texture,					ParserIntType, "sprite_id");
-			ParserObj_addElement(&obj, charac.battleScript ? charac.battleScript : "",	ParserStringType, "battle_info");
+			ParserObj_addElement(&obj, charac.name, ParserStringType, "name");
+			len = charac.movement.pos.x;
+			ParserObj_addElement(&obj, &len, ParserIntType, "x_pos");
+			len = charac.movement.pos.y;
+			ParserObj_addElement(&obj, &len, ParserIntType, "y_pos");
+			ParserObj_addElement(&obj, &charac.texture, ParserIntType, "sprite_id");
+			ParserObj_addElement(&obj, charac.battleScript ? charac.battleScript : "", ParserStringType, "battle_info");
 			ParserList_addElement(result, &obj, ParserObjType, -1);
 		}
 		buffer = concatf("%s/characters-save.json", game->loadedMap);
