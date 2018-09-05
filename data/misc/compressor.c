7e38947f831b11817a322e62b3ab3b9dd8e2af10
Gegel85 <Gegel85@laposte.net>
2018-09-05 23:08:41 +0200

    Changed compressor and uncompressor

--- data/misc/compressor.c
+++ data/misc/compressor.c
@@ -4,45 +4,54 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **args)
 {
-	int			len = 0;
-	int			read_chars = 0;
+	int		leng = 0;
+	int		read_chars = 0;
 	struct stat	file_info;
 	char		*buffer = 0;
-	int			fd = 0;
-	int			file = 0;
+	int		fd = 0;
+	int		file = 0;
+	FILE		*stream1;
+	FILE		*stream2;
+	char		len = 0;
 
-	file = open("compressed_files", O_WRONLY | O_CREAT, 0766);
-	if (file < 0) {
+	stream1 = fopen("compressed_files", "wb");
+	if (!stream1) {
 		perror("compressed_files");
-		exit(84);
+		return (EXIT_FAILURE);
 	}
+	file = fileno(stream1);
 	for (int i = 1; i < argc; i++) {
 		if (stat(args[i], &file_info) < 0) {
 			perror(args[i]);
 		} else {
-			len = file_info.st_size;
-			buffer = malloc(len);
+			leng = file_info.st_size;
+			buffer = malloc(leng);
 			if (buffer == 0) {
-				printf("Error couldn't malloc %iB\n", len);
+				printf("Error couldn't malloc %iB\n", leng);
 			} else {
-				fd = open(args[i], O_RDONLY);
-				if (fd < 0) {
+				stream2 = fopen(args[i], "rb");
+				if (!stream2) {
 					perror(args[i]);
 				} else {
+					fd = fileno(stream2);
 					read_chars = 1;
-					while (read_chars != 0) {
-						read_chars = read(fd, buffer, len);
-					}
-					close(fd);
-					write(file, buffer, len);
-					write(file, "\n--end\n", 7);
+					while (read_chars != 0)
+						read_chars = read(fd, buffer, leng);
+					fclose(stream2);
+					write(file, buffer, leng);
+					write(file, "\n--end", 6);
+					len = strlen(args[i]);
+					write(file, &len, 1);
+					write(file, args[i], len);
 				}
 				free(buffer);
 			}
 		}
 	}
-	close(file);
+	fclose(stream1);
+	return (EXIT_SUCCESS);
 }
--- data/misc/uncompressor.c
+++ data/misc/uncompressor.c
@@ -19,59 +19,71 @@ int match(char *str, char *str2, int pos)
 
 int main(int argc, char **args)
 {
-	int			len = 0;
-	int			begin = 0;
-	int			pos = 0;
+	int		leng = 0;
+	int		begin = 0;
+	int		pos = 0;
 	struct stat	file_info;
 	char		*buffer = 0;
-	int			fd = 0;
-	int			file = 0;
-	int			file_nb = 0;
-	int			cur_file = 0;
+	int		fd = 0;
+	int		file = 0;
+	int		file_nb = 0;
+	int		cur_file = 0;
+	FILE		*stream1;
+	FILE		*stream2;
+	char		len = 0;
+	char		*name = NULL;
 
 	if (argc > 1)
 		file_nb = atoi(args[1]);
 	if (stat("compressed_files", &file_info) < 0) {
 		perror("compressed_files");
-		return (84);
+		return (EXIT_FAILURE);
 	}
-	len = file_info.st_size;
-	buffer = malloc(len);
-	if (buffer == 0) {
-		printf("Error couldn't malloc %iB\n", len);
-	} else {
-		file = open("compressed_files", O_RDONLY);
-		if (file < 0) {
-			perror("compressed_files");
-			exit(84);
-		} else {
-			printf("%i, %i, %p, %i\n", read(file, buffer, len), file, buffer, len);
-			printf("Fetching file n%i\n", file_nb);
-			printf("%i : %i && %i\n", cur_file < file_nb && buffer[pos], cur_file < file_nb, buffer[pos]);
-			while (pos < len) {
-				if (match(buffer, "--end", pos)) {
-					printf("Found it !\n");
-					if(cur_file >= file_nb - 1) {
-						break;
-					}
-					if (cur_file <= file_nb - 2) {
-						begin = pos + 6;
-						cur_file++;
-					}
+	leng = file_info.st_size;
+	buffer = malloc(leng);
+	if (!buffer) {
+		printf("Error couldn't malloc %iB\n", leng);
+		return (EXIT_FAILURE);
+	}
+	stream1 = fopen("compressed_files", "rb");
+	if (!stream1) {
+		perror("compressed_files");
+		return (EXIT_FAILURE);
+	}
+	file = fileno(stream1);
+	printf("%i, %i, %p, %i\n", read(file, buffer, leng), file, buffer, leng);
+	printf("Fetching file n°%i\n", file_nb);
+	while (pos < leng) {
+		if (match(buffer, "\n--end", pos)) {
+			printf("Found it !\n");
+			len = buffer[pos + 6];
+			if(cur_file >= file_nb - 1) {
+				name = malloc(len);
+				if (!name) {
+					printf("Error couldn't alloc %iB\n", len);
+					return (EXIT_FAILURE);
 				}
-				pos++;
+				for (int i = 0; i < len && buffer[pos + 7 + i]; i++)
+					name[i] = buffer[pos + 7 + i];
+				name[len] = 0;
+				break;
+			} else if (cur_file <= file_nb - 2) {
+				begin = pos + 7 + len;
+				cur_file++;
 			}
-			fd = open("file", O_WRONLY | O_CREAT, 0766);
-			if (fd < 0) {
-				perror("file");
-				free(buffer);
-				return (84);
-			}
-			printf("Writing from %i to %i\n", begin, pos);
-			write(fd, buffer + begin, pos - begin - 2);
-			close(fd);
 		}
-		free(buffer);
+		pos++;
 	}
-	close(file);
+	stream2 = fopen(name, "wb");
+	if (!stream2) {
+		perror(name);
+		free(buffer);
+		return (EXIT_FAILURE);
+	}
+	fd = fileno(stream2);
+	printf("Writing from %i to %i\n", begin, pos);
+	write(fd, buffer + begin, pos - begin - 2);
+	fclose(stream2);
+	fclose(stream1);
+	free(buffer);
 }
