91d3fea36cfbe180705c20ca7302f06227d4ca73
Gegel85 <Gegel85@laposte.net>
2018-09-27 16:28:23 +0200

    Output is dumped in a log file when not in debug mode

--- src/main.c
+++ src/main.c
@@ -49,8 +49,11 @@ void	closeConsole(bool debug)
 	#if defined _WIN32 || defined __WIN32 || defined __WIN32__
 	if (!debug && !FreeConsole())
 		printf("%s: Cannot close main console\n", ERROR_BEG);
-	else if (debug)
+	else if (debug) {
 		AllocConsole();
+		freopen("CONOUT$", "w", stdout);
+	} else
+		freopen("last.log", "w", stdout);
 	#else
 	(void)debug;
 	#endif
